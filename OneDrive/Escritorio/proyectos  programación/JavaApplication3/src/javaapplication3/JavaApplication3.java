e04828cb943731c42f34a5eff88a92649e1d776c
aitor <aitor@172.16.4.78>
2020-10-08 17:54:33 +0200

    Hola mundo

--- OneDrive/Escritorio/proyectos  programación/JavaApplication3/src/javaapplication3/JavaApplication3.java
+++ OneDrive/Escritorio/proyectos  programación/JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -15,7 +15,7 @@ public class JavaApplication3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola soy Aitor Parada Linaje");
         // TODO code application logic here
     }
     
