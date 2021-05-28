2d0de4d909af2d4d01b412a23f1ec4a6bfc7c92b
Aitor <Aitor@DESKTOP-0JMUN33>
2021-05-28 21:18:00 +0200

    Cambios en Mantenimiento DAO y creacion de interfaz gráfica .

---
+++ ReparacionEquipos/src/dao/EquipoDao.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.sql.Connection;
+import Entidades.Equipo;
+import ConexionBD.ConexionBD;
+import Entidades.Empleado;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Rxndy
+ */
+public class EquipoDao {
+    
+}
